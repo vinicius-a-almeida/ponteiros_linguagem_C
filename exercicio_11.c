baf1ce9c243b34362f5228db35ef34150d6a2f4d
unknown <vinicius45anacleto@gmail.com>
2022-06-07 20:08:12 -0300

    commit

--- exercicio_11.c
+++ exercicio_11.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
 int main(){
+    int vet[5], i;
+    int *p_vet = &vet[0];
+    for(i = 0; i <= 4; i++){
+        scanf("%d", (vet + i));
+    }
+    printf("-----------------------------\n");
+    for(i = 0; i <= 4; i++){
+      if(*(p_vet + i) % 2 == 0){
+          printf("%p\n", (p_vet + i));
+      }  
+    }
     
 }
